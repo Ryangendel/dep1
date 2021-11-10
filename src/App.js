4c45f3e84ae9ae2cf8aee528564ead2be6596c0c
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-11-10 13:59:31 -0700

    message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
+import react, {useEffect} from "react"
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(()=>{
+    axios.get("/bulldog")
+    .then(data=>{
+      console.log("+++++++")
+      console.log(data)
+      console.log("+++++++")
+    })
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload DUDE.
         </p>
         <a
           className="App-link"
